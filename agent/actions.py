ea45e37a9024d14fa056d8f3cf25b26225c83cb1
bombk <bomncit@gmail.com>
2025-12-25 01:31:08 +0545

    update

--- agent/actions.py
+++ agent/actions.py
@@ -4,7 +4,7 @@ import logging
 def restart_heavy_process():
     for p in psutil.process_iter(['pid', 'cpu_percent', 'name']):
         try:
-            if p.info['cpu_percent'] and p.info['cpu_percent'] > 50:
+            if p.info['cpu_percent'] and p.info['cpu_percent'] > 20:
                 logging.warning(f"Killing process {p.info['name']} (PID {p.pid})")
                 p.terminate()
                 break
